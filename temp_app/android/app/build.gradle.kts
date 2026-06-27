b4c01997fd8fd62333052023c5dc096011f691f7
tamilarasu <tamilarasu@botminds.ai>
2026-06-27 12:46:05 +0530

    feat: initialize new Flutter project with multi-platform scaffold

--- temp_app/android/app/build.gradle.kts
+++ temp_app/android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.moonleaf.moonleaf"
+    namespace = "com.example.temp_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,8 +15,8 @@ android {
     }
 
     defaultConfig {
-        // Unique Play Store Application ID for Moonleaf.
-        applicationId = "com.moonleaf.reader"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.temp_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
